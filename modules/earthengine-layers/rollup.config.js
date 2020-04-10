90f1b3603232936a5374f85219088de0a36da533
Shan He <heshan0131@gmail.com>
2020-04-10 09:51:14 -0700

    initial rollup build config (#10)
    * using rollup to build
    
    Signed-off-by: Shan He <heshan0131@gmail.com>
    
    * rollup.config
    
    Signed-off-by: Shan He <heshan0131@gmail.com>
    
    * add browser: true
    
    Signed-off-by: Shan He <heshan0131@gmail.com>

---
+++ modules/earthengine-layers/rollup.config.js
@@ -0,0 +1,28 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+const config = {
+  input: 'src/bundle.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+    name: 'customLayerLibrary',
+    globals: {
+      '@deck.gl/core': 'deck',
+      '@deck.gl/layers': 'deck',
+      '@luma.gl/core': 'luma'
+    }
+  },
+  external: ['@deck.gl/core', '@deck.gl/layers', '@luma.gl/core'],
+  plugins: [
+    resolve({
+      browser: true,
+      preferBuiltins: true
+    }),
+    commonjs(),
+    json()
+  ]
+};
+
+export default config;
