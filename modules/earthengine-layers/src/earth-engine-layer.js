fffe42d7976c679e6764e79aeabe1538a3a8619e
Kyle Barron <kylebarron2@gmail.com>
2020-05-12 16:40:28 -0600

    Remove custom getMapAsync (#52)

--- modules/earthengine-layers/src/earth-engine-layer.js
+++ modules/earthengine-layers/src/earth-engine-layer.js
@@ -5,7 +5,7 @@ import EEApi from './ee-api'; // Promisify ee apis
 import ee from '@google/earthengine';
 import {load} from '@loaders.gl/core';
 import {ImageLoader} from '@loaders.gl/images';
-import {deepEqual} from './utils';
+import {deepEqual, promisifyEEMethod} from './utils';
 
 const eeApi = new EEApi();
 // Global access token, to allow single EE API initialization if using multiple
@@ -80,7 +80,7 @@ export default class EarthEngineLayer extends CompositeLayer {
     }
 
     // Evaluate map
-    const map = await eeObject.getMapAsync(props.visParams);
+    const map = await promisifyEEMethod(eeObject, 'getMap', props.visParams);
 
     // Get a tile url generation function
     const getTileUrl = map.formatTileUrl.bind(map);
--- modules/earthengine-layers/src/utils.js
+++ modules/earthengine-layers/src/utils.js
@@ -19,3 +19,16 @@ export function deepEqual(a, b) {
   }
   return true;
 }
+
+// Promisify eeObject methods
+export function promisifyEEMethod(eeObject, method, ...args) {
+  return new Promise((resolve, reject) =>
+    eeObject[method](...args, (value, error) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(value);
+    })
+  );
+}
