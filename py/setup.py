16a8f101caaaa7dcd9fc36658a22c0dd5593ec19
Kyle Barron <kylebarron2@gmail.com>
2020-05-26 18:56:01 -0600

    Bump version

--- py/setup.py
+++ py/setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/UnfoldedInc/earthengine-layers/blob/master/py',
-    version='0.1.1',
+    version='0.2.0',
     zip_safe=False,
 )
