01147aea4a65075e984e280a74842a8a83af51c8
Kyle Barron <kylebarron2@gmail.com>
2020-06-01 19:42:29 -0600

    Use function for ee API initialization (#73)
    * Use function for ee API initialization
    
    * Update initializeEEApi name

--- modules/earthengine-layers/src/ee-api.js
+++ modules/earthengine-layers/src/ee-api.js
@@ -1,52 +1,46 @@
 import ee from '@google/earthengine';
 
-export default class EEApi {
-  constructor() {
-    this.ee = ee;
+export async function initializeEEApi({clientId, token}) {
+  if (token) {
+    setToken(token);
+  } else {
+    await authenticateViaOAuth(clientId);
   }
+  await _initialize();
+}
 
-  async initialize({clientId, token}) {
-    if (token) {
-      this.setToken(token);
-    } else {
-      await this.authenticateViaOAuth(clientId);
-    }
-    await this._initialize();
+// Authenticate using an OAuth pop-up.
+async function authenticateViaOAuth(clientId, extraScopes = null) {
+  return await new Promise((resolve, reject) => {
+    ee.data.authenticateViaOauth(
+      clientId,
+      value => resolve(value),
+      error => reject(error),
+      extraScopes,
+      value => {
+        // called if automatic behind-the-scenes authentication fails
+        // console.debug('EE authentication opening popup', value);
+        ee.data.authenticateViaPopup(resolve, reject);
+      }
+    );
+  });
+}
+
+// Set short-lived Access Token directly
+function setToken(token) {
+  if (token) {
+    ee.apiclient.setAuthToken('', 'Bearer', token, 3600, [], undefined, false);
   }
+}
 
-  // Authenticate using an OAuth pop-up.
-  async authenticateViaOAuth(clientId, extraScopes = null) {
-    return await new Promise((resolve, reject) => {
-      ee.data.authenticateViaOauth(
-        clientId,
-        value => resolve(value),
-        error => reject(error),
-        extraScopes,
-        value => {
-          // called if automatic behind-the-scenes authentication fails
-          // console.debug('EE authentication opening popup', value);
-          ee.data.authenticateViaPopup(resolve, reject);
-        }
-      );
-    });
-  }
+// Initialize client library
+async function _initialize(baseurl = null, tileurl = null) {
+  // TODO initialize seems to need ee to be set on global window object
+  // We may be importing in non-standard way...?
+  /* global window */
+  window.ee = ee;
 
-  // Set short-lived Access Token directly
-  setToken(token) {
-    if (token) {
-      ee.apiclient.setAuthToken('', 'Bearer', token, 3600, [], undefined, false);
-    }
-  }
-
-  // Initialize client library
-  async _initialize(baseurl = null, tileurl = null) {
-    // TODO initialize seems to need ee to be set on global window object
-    // We may be importing in non-standard way...?
-    /* global window */
-    window.ee = ee;
-
-    return await new Promise((resolve, reject) => {
-      ee.initialize(baseurl, tileurl, value => resolve(value), error => reject(error));
-    });
-  }
+  return await new Promise((resolve, reject) => {
+    ee.initialize(baseurl, tileurl, value => resolve(value), error => reject(error));
+  });
 }
