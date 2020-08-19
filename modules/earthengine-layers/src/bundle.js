164bd533a6c2a620481e6902972a71fc2342f7d1
Kyle Barron <kylebarron2@gmail.com>
2020-08-18 18:12:53 -0600

    Add EarthEngineTerrainLayer to global

--- modules/earthengine-layers/src/bundle.js
+++ modules/earthengine-layers/src/bundle.js
@@ -1,7 +1,9 @@
 /* global window, global */
 import EarthEngineLayer from './earth-engine-layer';
+import EarthEngineTerrainLayer from './earth-engine-terrain-layer';
 const EarthEngineLayerLibrary = {
-  EarthEngineLayer
+  EarthEngineLayer,
+  EarthEngineTerrainLayer
 };
 
 const _global = typeof window === 'undefined' ? global : window;
